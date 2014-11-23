b5d8a731eaee9b408cfa2fc9c16a1c93b97f40f9
ramanbut-husaim <raman_but-husaim@live.com>
2014-11-23 14:42:09 +0300

    [Lab02]: Add two tests.

--- lab02/test/lab02/core_test.clj
+++ lab02/test/lab02/core_test.clj
@@ -1,7 +1,23 @@
 (ns lab02.core-test
   (:require [clojure.test :refer :all]
-            [lab02.core :refer :all]))
+            [lab02.core :refer :all]
+            [net.cgrand.enlive-html :as html]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn load-formatted-content-from-file
+  [file-name]
+  (html/html-snippet (slurp file-name)))
+
+(deftest parse-html
+  (testing "Parse html page correctly."
+    (let [content (load-formatted-content-from-file "resources/index.html")
+          uris (get-valid-urls content)]
+      (is (= (count uris) 4)))
+  ) #_testing
+)
+
+(deftest page-not-found
+  (testing "Page not found"
+    (let [server-respone (fetch-url "http://295e492141d4c35d74f7f8e2fbd9301f.com")]
+      (is (= (:status server-respone) 404)))
+  ) #_testing
+)
